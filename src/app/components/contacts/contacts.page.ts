4d410220ae12da07c23206e8855783d599984ec8
Alison Orellana <ali15.orellan@gmail.com>
2022-01-15 11:53:52 -0400

    changes

--- src/app/components/contacts/contacts.page.ts
+++ src/app/components/contacts/contacts.page.ts
@@ -9,18 +9,39 @@ export class ContactsPage implements OnInit {
 
   constructor() { }
 
-  mensaje: string;
-  correo: string;
-  nombre: string;
-  msg = '';
+  public mensaje: string;
+  public correo: string;
+  public nombre: string;
+  public msg = '';
+  public list1: number[] = [1, 2, 3];
+  list2: any[] = [
+    {id: 1, nam:'Ali'},
+    {id: 2, nam:'Ric'}
+  ];
+
+  people: any[] = [
+    {
+      "name": "Douglas  Pace"
+    },
+    {
+      "name": "Mcleod  Mueller"
+    },
+    {
+      "name": "Day  Meyers"
+    },
+    {
+      "name": "Aguirre  Ellis"
+    },
+    {
+      "name": "Cook  Tyson"
+    }
+  ];
 
   ngOnInit() {
   }
   public click(){
-
     console.log( this.nombre, this.correo ,this.mensaje);
-    this.msg = 'Nombre: '+this.nombre+' \nCorreo: '+ this.correo+" \nMensaje: "+this.mensaje
-    alert("Usuario registrado:\n"+ this.msg);
-
+    this.msg = 'Nombre: '+this.nombre+' \nCorreo: '+ this.correo+ " \nMensaje: " +this.mensaje;
+    alert("Usuario registrado: \n"+ this.msg);
   }
 }
